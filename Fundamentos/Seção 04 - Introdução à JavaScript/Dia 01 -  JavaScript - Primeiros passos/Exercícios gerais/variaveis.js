c59e1e02ed05bc9488eaea0c0ebb330eef54facf
Carolina Silva <carolinasilvagc08@gmail.com>
2022-12-20 15:07:52 -0300

    resolução exercicio variaveis

---
+++ Fundamentos/Seção 04 - Introdução à JavaScript/Dia 01 -  JavaScript - Primeiros passos/Exercícios gerais/variaveis.js
@@ -0,0 +1,17 @@
+//Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina)
+const myName = 'Carolina';
+
+//Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = 'São Paulo';
+
+//Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1995;
+
+//Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName, birthCity, birthYear);
+
+//Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030;
+
+//Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+'Porque const não pode ser reatribuída, beijos.'

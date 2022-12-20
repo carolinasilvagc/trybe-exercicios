721e4c4319cb32e8672bd156e1514e68c4fcc344
Carolina Silva <carolinasilvagc08@gmail.com>
2022-12-20 16:12:11 -0300

    resolução exercício 2, agora a prática

---
+++ Fundamentos/Seção 04 - Introdução à JavaScript/Dia 01 -  JavaScript - Primeiros passos/Exercícios, agora a prática/exercicio-2.js
@@ -0,0 +1,12 @@
+//Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+
+const a = 3;
+const b = 2;
+
+if(a > b) {
+  console.log('A é maior do que B')
+} else if(a < b) {
+  console.log('A é menor do que B')
+} else if (a === b) {
+  console.log('A é igual a B')
+}

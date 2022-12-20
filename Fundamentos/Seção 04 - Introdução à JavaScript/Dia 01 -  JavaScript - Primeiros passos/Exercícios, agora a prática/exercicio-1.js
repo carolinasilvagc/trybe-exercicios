bbbd122812f2106742054c92fcb3ced6a76c4025
Carolina Silva <carolinasilvagc08@gmail.com>
2022-12-20 16:07:49 -0300

    resolução exercício 01, agora a prática

---
+++ Fundamentos/Seção 04 - Introdução à JavaScript/Dia 01 -  JavaScript - Primeiros passos/Exercícios, agora a prática/exercicio-1.js
@@ -0,0 +1,18 @@
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+const a = 5;
+const b = 7;
+
+//Adição (a + b)
+console.log(a + b);
+
+//Subtração (a - b)
+console.log(a - b);
+
+//Multiplicação (a * b)
+console.log(a * b);
+
+//Divisão (a / b)
+console.log(a / b);
+
+//Módulo (a % b)
+console.log(a % b);

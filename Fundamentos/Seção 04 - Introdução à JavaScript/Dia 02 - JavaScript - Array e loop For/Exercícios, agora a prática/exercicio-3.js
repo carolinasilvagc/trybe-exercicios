77531ac7375f45017c57f4666d9768e23d754bf5
Carolina Silva <carolinasilvagc08@gmail.com>
2022-12-22 09:15:38 -0300

    resolução exercício 3, agora a prática

--- Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/Exercícios, agora a prática/exercicio-3.js
+++ Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/Exercícios, agora a prática/exercicio-3.js
@@ -2,10 +2,11 @@
 //A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let media = 0;
+let soma = 0;
 
-for(index of numbers) {
-  media += (numbers[index] / numbers.length)
+for(let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i];
 }
 
+let media = soma / numbers.length;
 console.log(media)

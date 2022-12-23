7379d48d1b227c3a9822a2742d05f4e655791317
Carolina Silva <carolinasilvagc08@gmail.com>
2022-12-23 09:16:50 -0300

    resolução exercício maior nome

---
+++ Fundamentos/Seção 04 - Introdução à JavaScript/Dia 04 - JavaScript - Objetos e funções/Exercícios, agora a prática/Funções/maior-nome.js
@@ -0,0 +1,16 @@
+//Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+//Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+
+//Valor esperado no retorno da função: Fernanda.
+
+function findBiggestName(names) {
+  let biggestName = names[0];
+  for(let index in names) {
+    if(biggestName.length < names[index].length) {
+      biggestName = names[index];
+    }
+  }
+  return biggestName
+}
+console.log(findBiggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));

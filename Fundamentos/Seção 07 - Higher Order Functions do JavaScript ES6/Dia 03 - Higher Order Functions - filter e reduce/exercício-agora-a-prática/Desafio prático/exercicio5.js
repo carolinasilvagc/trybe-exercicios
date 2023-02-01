c4c39f3000aa9e5c43e8835093e621ea51a57269
Carolina Silva <carolinasilvagc08@gmail.com>
2023-02-01 17:53:00 -0300

    resolução exercícios agora a prática

---
+++ Fundamentos/Seção 07 - Higher Order Functions do JavaScript ES6/Dia 03 - Higher Order Functions - filter e reduce/exercício-agora-a-prática/Desafio prático/exercicio5.js
@@ -0,0 +1,18 @@
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+// 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+// De olho na dica 👀: você pode utilizar o map para iterar no array e utilizar o reduce para calcular a média.
+
+const studentAverage = () => {
+  return nameAndAverage = students.map((student, index) => (
+    {
+    name: student,
+    average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }
+  ));
+}
+
+console.log(studentAverage());

55fd44964c3d22bdf11b20e656409bc0abac5354
Carolina Silva <carolinasilvagc08@gmail.com>
2023-02-02 11:31:11 -0300

    resolução exercícios dia 04 e criação de pastas pro dia 05

---
+++ Fundamentos/Seção 07 - Higher Order Functions do JavaScript ES6/Dia 04 - JavaScript ES6 - spread operator, rest parameters e object destructuring/exercícios-agora-a-prática/exercicio4.js
@@ -0,0 +1,37 @@
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+
+];
+
+// Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const filterPeople = (peopleList) => {
+  return peopleList
+    .filter((person) => person.nationality === 'Australian' && person.bornIn >= 1901 && person.bornIn <= 2000);
+}
+
+console.log(filterPeople(people));

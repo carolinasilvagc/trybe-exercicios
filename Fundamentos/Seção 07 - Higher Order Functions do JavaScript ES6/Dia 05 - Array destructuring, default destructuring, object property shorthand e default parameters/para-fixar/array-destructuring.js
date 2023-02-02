b08c875f5c4b0eff240a4ee9b2db33aa6e375301
Carolina Silva <carolinasilvagc08@gmail.com>
2023-02-02 15:20:44 -0300

    resolução exercícios do dia 05

--- Fundamentos/Seção 07 - Higher Order Functions do JavaScript ES6/Dia 05 - Array destructuring, default destructuring, object property shorthand e default parameters/para-fixar/array-destructuring.js
+++ Fundamentos/Seção 07 - Higher Order Functions do JavaScript ES6/Dia 05 - Array destructuring, default destructuring, object property shorthand e default parameters/para-fixar/array-destructuring.js
@@ -10,6 +10,9 @@ sum(primeNumbers[0], primeNumbers[2]) // 54
 
 // Resposta:
 
+const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
+sum(firstNumber, thirdNumber);
+
 // 2. Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo.
 
 // A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
@@ -22,10 +25,16 @@ console.log(comida, animal, bebida); // arroz gato água
 
 // Resposta:
 
+[comida, animal, bebida] = [bebida, comida, animal]
+console.log(comida, animal, bebida)
+
 // 3. O array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares.
 
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-console.log(numerosPares); // [6, 8, 10, 12];
-
 // Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+
+// Resposta:
+
+[,,, ...numerosPares] = numerosPares;
+console.log(numerosPares);
